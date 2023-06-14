536d4ae8afa4e644ff1f3d5637d2e00175088589
Felipe Oliveira <felpd3v@gmail.com>
2023-06-14 11:42:36 -0300

    chore: spread sjc crawler into a file

---
+++ src/crawlers/sjc.crawler.ts
@@ -0,0 +1,47 @@
+import { Page } from 'puppeteer'
+import * as urls from '../constants/urls.constants'
+import { delay } from '../utils/wait'
+import env from '../config/environment'
+
+export const login = async (page: Page): Promise<void> => {
+	try {
+		await page.goto(urls.sjc.auth)
+		await page.setViewport({ width: 1080, height: 1024 })
+
+		console.log('Filling up auth fields')
+		await page.type('#input-56', env.auth_login)
+		await page.type('#input-61', env.auth_password)
+
+		console.log('Signing in')
+		await page.click('.v-card__actions button')
+
+		console.log('Waiting for a page load')
+		await page.waitForNavigation()
+
+		console.log('Page load: Authenticated')
+		const header = await page.waitForSelector('.profile-picture')
+		// const headerTextContent = await header?.evaluate((el) => el.textContent)
+	} catch (error) {
+		console.error('Crawler SJC Login Error:', error)
+		throw new Error('Crawler SJC Login Error')
+	}
+}
+
+export const logout = async (page: Page): Promise<void> => {
+	try {
+		console.log('Clicking on exit button')
+		await page.click('#navbartop li a.botaoSairMenu')
+
+		console.log('Waiting for confirmation modal')
+		await page.waitForSelector('.swal-modal .swal-footer .swal-button--confirm')
+
+		console.log('Clicking on confirmation button')
+		await page.click('.swal-button--confirm')
+
+		console.log('Waiting for homepage')
+		await page.waitForSelector('[role="document"]')
+	} catch (error) {
+		console.error('Crawler SJC Logout Error:', error)
+		throw new Error('Crawler SJC Logout Error')
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,18 @@
 import 'dotenv/config'
 import puppeteer from 'puppeteer'
-import * as urls from './constants/urls.constants'
-import { delay } from './utils/wait'
-import env from './config/environment'
+import * as SJC from './crawlers/sjc.crawler'
 ;(async () => {
 	const browser = await puppeteer.launch({ headless: false })
 	const page = await browser.newPage()
 
-	await page.goto(urls.sjc.auth)
-	await page.setViewport({ width: 1080, height: 1024 })
+	try {
+		await SJC.login(page)
+		await SJC.logout(page)
 
-	console.log('Filling up auth fields')
-	await page.type('#input-56', env.auth_login)
-	await page.type('#input-61', env.auth_password)
-
-	console.log('Signing in')
-	await page.click('.v-card__actions button')
-
-	console.log('Waiting for a page load')
-	await page.waitForNavigation()
-
-	console.log('Page load: Authenticated')
-	const header = await page.waitForSelector('.profile-picture')
-	const headerTextContent = await header?.evaluate((el) => el.textContent)
-
-	console.log('Clicking on exit button')
-	await page.click('#navbartop li a.botaoSairMenu')
-
-	console.log('Waiting for confirmation modal')
-	await page.waitForSelector('.swal-modal .swal-footer .swal-button--confirm')
-
-	console.log('Clicking on confirmation button')
-	await page.click('.swal-button--confirm')
-
-	console.log('Waiting for homepage')
-	await page.waitForSelector('[role="document"]')
-
-	await browser.close()
-
-	console.log('Done, browser closed')
+		console.log('Done!')
+	} catch (error) {
+		console.log('Crawler error:', error)
+	} finally {
+		await browser.close()
+	}
 })()
