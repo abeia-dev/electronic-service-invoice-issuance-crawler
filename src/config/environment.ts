97d11880af70c7e85035827ec349468578f4e34e
Felipe Oliveira <felpd3v@gmail.com>
2023-06-18 17:35:56 -0300

    chore: setup the project env var

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,8 +1,1 @@
-const { AUTH_LOGIN, AUTH_PASSWORD } = process.env
-
-const env = {
-	auth_login: `${AUTH_LOGIN}`,
-	auth_password: `${AUTH_PASSWORD}`,
-}
-
-export default env
+export const SERVER_PORT = process.env.SERVER_PORT
